8cf5dec3245272dfa2138e41719e48d145f130a4
Stijn Hemeltjen <0340955@student.rocvantwente.nl>
2023-05-11 14:01:22 +0200

    Feat: Added code to buttonscript
    Made the 3 buttons in the StartScreenScene work, they do have to be set to the correct scenes.

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -1,8 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonScript : MonoBehaviour
 {
+    public void StartGame()
+    {
+        SceneManager.LoadScene(0); // SampleScene Scene gets loaded (Game)
+    }
 
+    //public void Restart()
+    //{
+    //    SceneManager.LoadScene(2); // MainMenu Scene gets loaded
+    //}
+
+    public void InsertCoins()
+    {
+        SceneManager.LoadScene(0); // MainMenu Scene gets loaded
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
