ef8aeb44c1c2ff1e8eed35fa96927ea13410dc5c
Koen Polman <KoenPolman_@outlook.com>
2023-05-17 14:46:29 +0200

    feat:gameplay
    het pad herhaalt en is wat langer

--- Assets/Scripts/GridBehavior.cs
+++ Assets/Scripts/GridBehavior.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class GridBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float velocity = 10;
+    [SerializeField] private float expirationDate = 1500;
+    private float lifeSpan = 0;
+    [SerializeField] private Rigidbody2D rb;
     void Start()
     {
-        
+        rb.AddForce(new Vector3(0, (-1 * velocity), 0));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(lifeSpan > expirationDate)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void FixedUpdate()
+    {
+        lifeSpan++;
     }
 }
---
+++ Assets/Scripts/pathRepeater.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pathRepeater : MonoBehaviour
+{
+    [SerializeField] GameObject Grid = null;
+    private float lifeSpan = 1;
+    private float interval = 1250;
+    private void FixedUpdate()
+    {
+        if (lifeSpan % interval == 0)
+        {
+            GameObject newGrid = Instantiate(Grid);
+            newGrid.transform.position = new Vector3(-0.5625f, 31f, 0f);
+        }
+        lifeSpan++;
+    }
+}
