5911ff158a15375b5e78db2dabd8195801833a91
Koen Polman <KoenPolman_@outlook.com>
2023-05-24 10:42:15 +0200

    Feat: proggress with obstacles

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -9,34 +9,44 @@ public class ObstacleSpawner : MonoBehaviour
     [SerializeField] GameObject Bottle = null; 
     [SerializeField] GameObject PizzaAndCan = null; 
     [SerializeField] GameObject Stump = null; 
-    [SerializeField] GameObject Patoo1 = null; 
+    [SerializeField] GameObject Patoo1 = null;
     [SerializeField] GameObject Patoo2 = null;
-    private float lifeSpan = -400;
+    [SerializeField] GameObject Clock = null; 
+    [SerializeField] GameObject Potion = null;
+
+    private float timeBetweenObstacles = -400;
     private float maxSpaceBetweenObstacles = 400;
     private void FixedUpdate()
     {
         float num = Random.Range(1, maxSpaceBetweenObstacles);
-        if (num <= lifeSpan)
+        if (num <= timeBetweenObstacles)
         {
             float obstaclePicker = Random.Range(1, 8);
             switch (obstaclePicker)
             {
                 case 1:
+                    GameObject newObstacle = Instantiate(Dumpster);
                     break;
                 case 2:
+                    GameObject newObstacle = Instantiate(Dumpster);
                     break;
                 case 3:
+                    GameObject newObstacle = Instantiate(Dumpster);
                     break;
                 case 4:
+                    GameObject newObstacle = Instantiate(Dumpster);
                     break;
                 case 5:
+                    GameObject newObstacle = Instantiate(Dumpster);
                     break;
                 case 6:
+                    GameObject newObstacle = Instantiate(Dumpster);
                     break;
                 case 7:
+                    GameObject newObstacle = Instantiate(Dumpster);
                     break;
             }
-            lifeSpan = -50;
+            timeBetweenObstacles = -50;
             maxSpaceBetweenObstacles = maxSpaceBetweenObstacles - 10;
         }
         lifeSpan++;
