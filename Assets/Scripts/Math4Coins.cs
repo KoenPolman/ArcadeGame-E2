e88d597360082638b4833c54b957c5a88489cc43
Koen Polman <KoenPolman_@outlook.com>
2023-05-25 10:09:51 +0200

    fix: scene was gone but now not

--- Assets/Scripts/Math4Coins.cs
+++ Assets/Scripts/Math4Coins.cs
@@ -4,167 +4,127 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CoinGame : MonoBehaviour
+public class Math4Coins : MonoBehaviour
 {
-<<<<<<< Updated upstream
     [SerializeField] Text coinText;
-    void Start()
-    {
-                
-=======
     public Text text;
     public Text answers;
     [SerializeField] Text[] answersArr;
-
-        int wrong1;
-        int wrong2;
-        int wrong3;
-        int correct;
-
-   void UpdateAnswers()
-    {
-        answersArr[0].text = wrong1.ToString();
-        answersArr[1].text = wrong2.ToString();
-        answersArr[2].text = wrong3.ToString();
-        answersArr[3].text = correct.ToString();
-
-    }
-
+    int wrong1;
+    int wrong2;
+    int wrong3;
+    int correct;
     void Start()
     {
-
         string right;
         int notCorrect1 = GetNum();
         int notCorrect2 = GetNum();
         string wrong;
         string sum = "";
-
         int num1 = GetNum();
         int num2 = GetNum();
-
         Input.GetMouseButton(1);
-
-
-
         int opp = Random.Range(1, 3);
-
-
         if (opp == 1)
         {
             if (num1 > num2)
             {
-                 sum = num1.ToString() + "-" + num2.ToString();
-               
-
-
-
+                sum = num1.ToString() + "-" + num2.ToString();
                 correct = num1 - num2;
-             
+
                 if (notCorrect1 > GetNum())
                 {
                     right = correct.ToString();
-                 
+
                     wrong1 = notCorrect1 - GetNum();
                     wrong = wrong1.ToString();
-                   
+
                     wrong2 = notCorrect1 - GetNum();
                     wrong = wrong2.ToString();
-                  
+
 
                     wrong3 = notCorrect1 - GetNum();
                     wrong = wrong3.ToString();
-                  
-
-
                 }
                 else if (notCorrect2 > GetNum())
                 {
                     right = correct.ToString();
-                 
+
                     wrong1 = notCorrect2 - GetNum();
                     wrong = wrong1.ToString();
-                   
+
                     wrong2 = notCorrect2 - GetNum();
                     wrong = wrong2.ToString();
-                  
+
                     wrong3 = notCorrect2 - GetNum();
                     wrong = wrong3.ToString();
-                   
-
                 }
 
             }
             else if (num1 < num2)
             {
                 sum = num2.ToString() + "-" + num1.ToString();
-              
+
                 correct = num2 - num1;
-             
+
                 if (notCorrect1 > GetNum())
                 {
                     right = correct.ToString();
-                  
+
                     wrong1 = notCorrect1 - GetNum();
                     wrong = wrong1.ToString();
-                   
+
                     wrong2 = notCorrect1 - GetNum();
                     wrong = wrong2.ToString();
-                  
+
                     wrong3 = notCorrect1 - GetNum();
                     wrong = wrong3.ToString();
-                   
                 }
                 else if (notCorrect2 > GetNum())
                 {
                     right = correct.ToString();
-                  
+
                     wrong1 = notCorrect2 - GetNum();
                     wrong = wrong1.ToString();
-                   
+
                     wrong2 = notCorrect2 - GetNum();
                     wrong = wrong2.ToString();
-                 
+
                     wrong3 = notCorrect2 - GetNum();
                     wrong = wrong3.ToString();
-                    
                 }
 
             }
         }
         else if (opp == 2)
         {
-         sum = num1.ToString() + "+" + num2.ToString();
+            sum = num1.ToString() + "+" + num2.ToString();
             correct = num1 + num2;
             right = correct.ToString();
-           
+
             wrong1 = notCorrect1 + GetNum();
             wrong = wrong1.ToString();
-           
+
             wrong2 = notCorrect1 + GetNum();
             wrong = wrong2.ToString();
-           
+
             wrong3 = notCorrect1 + GetNum();
             wrong = wrong3.ToString();
-           
-
         }
-            text.text = sum;
+        text.text = sum;
         UpdateAnswers();
 
     }
-
     int GetNum()
     {
         int num = Random.Range(100, 1000);
         return num;
-
-
-
->>>>>>> Stashed changes
     }
-    
-
-
-
+    void UpdateAnswers()
+    {
+        answersArr[0].text = wrong1.ToString();
+        answersArr[1].text = wrong2.ToString();
+        answersArr[2].text = wrong3.ToString();
+        answersArr[3].text = correct.ToString();
+    }
 }
-
