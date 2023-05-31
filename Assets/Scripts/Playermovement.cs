b4ff5b9baedca56396f538a7b173cf4ebe83dcda
LeonAssink <0347612@student.rocvantwente.nl>
2023-05-31 13:58:10 +0200

    Feat: Added scoring script

--- Assets/Scripts/Playermovement.cs
+++ Assets/Scripts/Playermovement.cs
@@ -1,9 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class Playermovement : MonoBehaviour
 {
+    public float timer;
+    public float score;
+    public float highScore;
     float playerposition = 0;
     [SerializeField] private Rigidbody2D rb;
     void Start()
@@ -29,5 +34,20 @@ public class Playermovement : MonoBehaviour
             playerposition = 2;       
         }
         transform.position = new Vector3(playerposition,-4,0);
+    }
+    void FixedUpdate()
+    {
+        timer += Time.deltaTime;
+        if (timer > 3)
+        {
+            PlayerPrefs.SetFloat("score: ", score);
+            score += 1;
+            timer = 0;
+            if (score > highScore)
+            {
+                highScore = score;
+                PlayerPrefs.SetFloat("highScore: ", MathF.Ceiling(highScore));
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Scoring.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class Scoring : MonoBehaviour
+{
+    public Text scoreText;
+    public Text highText;
+    public float timer;
+    public float score;
+    public float highScore;
+    void Start()
+    {
+        score = 0;
+        highScore = PlayerPrefs.GetFloat("highScore: ");
+        PlayerPrefs.SetFloat("score: ", score);
+        score = PlayerPrefs.GetFloat("score: ");
+    }
+    public void Update()
+    {
+        scoreText.text = score.ToString();
+        highText.text = highScore.ToString();
+        timer += Time.deltaTime;
+        if (timer > 3)
+        {
+            PlayerPrefs.SetFloat("score: ", score);
+            score += 1;
+            timer = 0;
+            if (score > highScore)
+            {
+                highScore = score;
+                PlayerPrefs.SetFloat("highScore: ", MathF.Ceiling(highScore));
+            }
+        }
+    }
+}
